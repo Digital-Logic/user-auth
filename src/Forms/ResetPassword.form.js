fb71fb616f34b36154784af50def2358afe0a7f7
Larry Brown <lbrown@digital-logic.net>
2019-05-05 17:20:29 -0700

    ResetPassword from created

--- src/Forms/ResetPassword.form.js
+++ src/Forms/ResetPassword.form.js
@@ -1,38 +1,25 @@
-import React, { useState } from 'react';
-import Button from '@material-ui/core/Button';
+import React, { useReducer } from 'react';
 import Grid from '@material-ui/core/Grid';
-import { Form, Input, isEmail, required } from '../UI/Forms';
-
+import { Form, Password, required, minLength } from '../UI/Forms';
+import Button from '@material-ui/core/Button';
 
 function ResetPassword({ onSubmit, onClose }) {
-    const [email, setEmail] = useState('');
-
     return (
-        <Form onSubmit={_onSubmit}>
-            <Input
-                label="eMail Address"
-                value={email}
-                onChange={onChange}
-                validate={[required(), isEmail()]}
+        <Form>
+            <Password
+                label="Password"
                 />
-
-            <Grid container justify="flex-end">
-                <Button type="submit" variant="outlined">Submit</Button>
+            <Password
+                label="Confirm Password"
+                />
+            <Grid container justify="space-between" alignItems="center">
+                <Button onClick={onClose}>close</Button>
+                <Button
+                    type="submit"
+                    variant="outlined">Submit</Button>
             </Grid>
         </Form>
     );
-
-    function  _onSubmit() {
-        onSubmit({
-            email
-        });
-    }
-
-    function onChange(event) {
-        const { value } = event.target;
-        setEmail(value);
-    }
 }
-
 
 export default ResetPassword;
