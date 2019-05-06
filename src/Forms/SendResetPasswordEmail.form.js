c38f3da400b99e0954595ce86875656b522d7e61
Larry Brown <lbrown@digital-logic.net>
2019-05-05 17:21:06 -0700

    Send Reset password email from created

---
+++ src/Forms/SendResetPasswordEmail.form.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import { Form, Input, isEmail, required } from '../UI/Forms';
+
+
+function SendResetPasswordEmail({ onSubmit }) {
+    const [email, setEmail] = useState('');
+
+    return (
+        <Form onSubmit={_onSubmit}>
+            <Input
+                label="eMail Address"
+                value={email}
+                onChange={onChange}
+                validate={[required(), isEmail()]}
+                />
+
+            <Grid container justify="flex-end">
+                <Button type="submit" variant="outlined">Submit</Button>
+            </Grid>
+        </Form>
+    );
+
+    function  _onSubmit() {
+        onSubmit({
+            email
+        });
+    }
+
+    function onChange(event) {
+        const { value } = event.target;
+        setEmail(value);
+    }
+}
+
+
+export default SendResetPasswordEmail;
